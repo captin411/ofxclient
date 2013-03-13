94caf148ab132f2f922a1bed2777f437ac00b725
David Bartle <captindave@gmail.com>
2013-03-12 23:25:05 -0700

    util function to allow combining ofx files into one

---
+++ ofxclient/util.py
@@ -0,0 +1,23 @@
+from request import Builder
+from StringIO import StringIO
+
+def combined_download(accounts,days=60):
+    """Download OFX files and combine them into one
+
+    It expects an 'accounts' list of ofxclient.Account objects
+    as well as an optional 'days' specifier which defaults to 60
+    """
+
+    builder = Builder(institution=None)
+
+    out_file = StringIO()
+    out_file.write(builder.header())
+    for a in accounts:
+        ofx = a.download(days=days).read()
+        stripped = ofx.partition('<OFX>')[2].partition('</OFX>')[0]
+        out_file.write(stripped)
+
+    out_file.write("<OFX>")
+    out_file.seek(0)
+
+    return out_file
